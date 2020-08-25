a9a5cff1300d30ceaa48f8fc7da251436ed1b145
Ishika Agarwal <agarwali@purdue.edu>
2020-08-24 22:00:10 -0700

    Improved Running Sum of 1D array

--- src/com/company/RunningSumOf1dArray.java
+++ src/com/company/RunningSumOf1dArray.java
@@ -8,10 +8,9 @@ public class RunningSumOf1dArray {
     public int[] runningSum(int[] nums) {
         int[] sums = new int[nums.length];
 
-        int currSum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            currSum += nums[i];
-            sums[i] = currSum;
+        sums[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sums[i] = sums[i - 1] + nums[i];
         }
 
         return sums;
