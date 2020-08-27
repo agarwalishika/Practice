5675ed25783980ae0073e25140d62e45f2bff8b0
Ishika Agarwal <agarwali@purdue.edu>
2020-08-26 19:32:18 -0700

    Number of Good Pairs

---
+++ src/com/company/DefangingAnIP.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/*
+https://leetcode.com/problems/defanging-an-ip-address/submissions/
+ */
+
+public class DefangingAnIP {
+    public String defangIPaddr(String address) {
+        for (int i = 0; i < address.length(); i++) {
+            if (address.charAt(i) == '.') {
+                String prefix = address.substring(0, i);
+                String suffix = address.substring(i + 1);
+                address = prefix + "[.]" + suffix;
+                i += 2;
+            }
+        }
+
+        return address;
+    }
+}
