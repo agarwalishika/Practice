5f7afd413060730cf6974a09ea19b3989a9efbbe
Ishika Agarwal <agarwali@purdue.edu>
2020-08-31 19:03:15 -0700

    decompress encoded list

---
+++ src/com/company/DecompressRunLengthEncodedList.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class DecompressRunLengthEncodedList {
+    public int[] decompressRLElist(int[] nums) {
+        int len = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            len += nums[i];
+        }
+
+        int[] list = new int[len];
+        int counter = 0;
+        for (int i = 0; i < nums.length && counter < list.length; i += 2) {
+            int freq = nums[i];
+            int val = nums[i + 1];
+            for (int j = 0; j < freq; j++){
+                list[counter] = val;
+                counter++;
+            }
+        }
+
+        return list;
+    }
+}
