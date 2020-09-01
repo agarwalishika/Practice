b07e7a890c99080652f511eaa2a76e6c258393c6
Ishika Agarwal <agarwali@purdue.edu>
2020-08-31 19:08:19 -0700

    xor operation in array

---
+++ src/com/company/XorOperationInAnArray.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class XorOperationInAnArray {
+    public int xorOperation(int n, int start) {
+        int temp = start;
+        for (int i = 1; i < n; i++) {
+            temp = temp ^ (start + 2 * i);
+        }
+
+        return temp;
+    }
+}
