49adf1b0dbfac2c3e0bf6ce737fd0fb8682db7cb
Ishika Agarwal <agarwali@purdue.edu>
2021-01-30 23:29:25 -0800

    problems

--- src/com/company/totalGain.java
+++ src/com/company/totalGain.java
@@ -1,12 +1,12 @@
 package com.company;
 
 public class totalGain {
-    public int largestAltitude(int[] gain) {
+    public int largestAltitude(int[] gains) {
         int max = 0;
         int total = 0;
 
-        for (int i = 0; i < gain.length; i++) {
-            total += gain[i];
+        for (int i = 0; i < gains.length; i++) {
+            total += gains[i];
             if (total > max) {
                 max = total;
             }
