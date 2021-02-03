6ef6a2d4c9639b5571788e710c1ed866cfdf3069
Ishika Agarwal <agarwali@purdue.edu>
2021-02-02 18:31:40 -0800

    problems

--- src/com/company/HowManyNumbersAreSmallerThanCurrent.java
+++ src/com/company/HowManyNumbersAreSmallerThanCurrent.java
@@ -6,7 +6,7 @@ https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-numb
 public class HowManyNumbersAreSmallerThanCurrent {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int[] mins = new int[nums.length];
-        for (int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i+=1){
             for (int j = 0; j < nums.length; j++) {
                 if (j != i && nums[j] < nums[i]) {
                     mins[i]++;
