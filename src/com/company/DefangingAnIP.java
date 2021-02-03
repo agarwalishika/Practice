d6051510f519870d0bde1c4d275e27178acdebea
Ishika Agarwal <agarwali@purdue.edu>
2021-02-02 18:31:31 -0800

    problems

--- src/com/company/DefangingAnIP.java
+++ src/com/company/DefangingAnIP.java
@@ -6,7 +6,7 @@ https://leetcode.com/problems/defanging-an-ip-address/submissions/
 
 public class DefangingAnIP {
     public String defangIPaddr(String address) {
-        for (int i = 0; i < address.length(); i++) {
+        for (int i = 0; i < address.length(); i+=1) {
             if (address.charAt(i) == '.') {
                 String prefix = address.substring(0, i);
                 String suffix = address.substring(i + 1);
