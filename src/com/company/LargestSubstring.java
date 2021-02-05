77eb1968c990d6de98864bd76238ba6a64922d59
Ishika Agarwal <agarwali@purdue.edu>
2021-02-05 08:51:10 -0800

    problems

--- src/com/company/LargestSubstring.java
+++ src/com/company/LargestSubstring.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class LargestSubstring {
     public static int maxLengthBetweenEqualCharacters(String s) {
-        int maxLen = -1;
+        int maxLen = -2;
         for (int i = 0; i < s.length(); i+=1) {
             char begc = s.charAt(i);
             int endc = s.lastIndexOf(begc);
